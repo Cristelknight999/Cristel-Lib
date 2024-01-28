667f5b6b900a847ce6c061769c4c9a7067a3f91c
unilock <unilock@fennet.rentals>
2024-01-28 18:34:14 -0500

    Fix compatibility with latest TerraBlender
    Signed-off-by: unilock <unilock@fennet.rentals>

--- common/src/main/java/net/cristellib/mixin/MixinTerrablenderConfig.java
+++ common/src/main/java/net/cristellib/mixin/MixinTerrablenderConfig.java
@@ -4,18 +4,17 @@ import net.cristellib.util.TerrablenderUtil;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import terrablender.config.Config;
 import terrablender.config.TerraBlenderConfig;
 
 @Mixin(value = TerraBlenderConfig.class, remap = false)
 public class MixinTerrablenderConfig {
-    @Redirect(method = "<init>", at = @At(value = "INVOKE", target = "Lterrablender/config/Config;addNumber(Ljava/lang/String;Ljava/lang/String;Ljava/lang/Number;Ljava/lang/Number;Ljava/lang/Number;)Ljava/lang/Number;"))
-    private <T extends Number & Comparable<T>> Number create(Config instance, String comment, String key, T defaultValue, T min, T max) {
+    @Redirect(method = "load()V", at = @At(value = "INVOKE", target = "terrablender/config/TerraBlenderConfig.addNumber(Ljava/lang/String;Ljava/lang/Number;Ljava/lang/Number;Ljava/lang/Number;Ljava/lang/String;)Ljava/lang/Number;"))
+    private <T extends Number & Comparable<T>> Number create(TerraBlenderConfig instance, String key, T defaultValue, T min, T max, String comment) {
         if (TerrablenderUtil.mixinEnabled() && key.equals("vanilla_overworld_region_weight")) {
             if (defaultValue instanceof Integer) {
                 return 0;
             }
         }
-        return instance.addNumber(comment, key, defaultValue, min, max);
+        return instance.addNumber(key, defaultValue, min, max, comment);
     }
 }
